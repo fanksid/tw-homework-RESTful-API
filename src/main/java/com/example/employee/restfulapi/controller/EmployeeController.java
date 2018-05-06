ff6173093979392ac6419b169e447e667bb66d22
superxc <superxcgm@gmail.com>
2018-05-06 16:17:09 +0800

    add comment

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -36,17 +36,36 @@ public class EmployeeController {
         return employeeRepository.findOne(id);
     }
 
+    /**
+     * Add a employee
+     *
+     * @param employee
+     * @return added employee
+     */
     @PostMapping(value = EMPLOYEE_URL_BASE)
     public Employee add(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    /**
+     * Update a employee
+     *
+     * @param id employee id
+     * @param employee new employee data
+     * @return updated employee
+     */
     @PutMapping(value = EMPLOYEE_URL_BASE + "/{id}")
     public Employee update(@PathVariable Long id, @RequestBody Employee employee) {
         employee.setId(id);
         return employeeRepository.save(employee);
     }
 
+    /**
+     * Delete a employee
+     *
+     * @param id employee id
+     * @return empty string
+     */
     @DeleteMapping(value = EMPLOYEE_URL_BASE + "/{id}")
     public String delete(@PathVariable Long id) {
         employeeRepository.delete(id);
